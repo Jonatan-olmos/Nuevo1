e57d4c7f08413a6ca8f89e0ccc11132ffc3c2410
jonatan-olmos <jon.ol.23.joo@gmail.com>
2023-09-14 20:06:51 -0300

    agregar titulo a read.md

--- array.js
+++ array.js
@@ -5,6 +5,6 @@ console.log(colores[3] );
 console.log(dato);//devuelve la cantidad total de elementos posterior  luego de aver agrgado push
 const datosEliminados = colores.pop();
 console.log(datosEliminados);
-console.log(colores);
+console.log(colores);*
 
 console.log(colores.includes("rojo")); */
